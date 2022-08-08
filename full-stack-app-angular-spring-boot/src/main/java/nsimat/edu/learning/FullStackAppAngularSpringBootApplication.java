6f7514ea42b44f51d18ac25ddce7240ccd735313
nsimat <nsimba_matondo@yahoo.fr>
2022-08-08 18:57:40 +0200

    First commit for fullstackappangularspringboot project

---
+++ full-stack-app-angular-spring-boot/src/main/java/nsimat/edu/learning/FullStackAppAngularSpringBootApplication.java
@@ -0,0 +1,13 @@
+package nsimat.edu.learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullStackAppAngularSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullStackAppAngularSpringBootApplication.class, args);
+	}
+
+}
---
+++ full-stack-app-angular-spring-boot/src/test/java/nsimat/edu/learning/FullStackAppAngularSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package nsimat.edu.learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullStackAppAngularSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
