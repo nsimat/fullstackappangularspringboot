c8a6469461bb605de80d4aeb33a1e9365e976c03
nsimat <nsimba_matondo@yahoo.fr>
2022-08-09 12:18:05 +0200

    Adding ReservationResource and ReservationResponse

---
+++ full-stack-app-angular-spring-boot/src/main/java/nsimat/edu/learning/model/response/ReservationResponse.java
@@ -0,0 +1,30 @@
+package nsimat.edu.learning.model.response;
+
+import org.w3c.dom.stylesheets.LinkStyle;
+
+import lombok.Getter;
+import lombok.Setter;
+import nsimat.edu.learning.model.Links;
+
+@Setter
+@Getter
+public class ReservationResponse {
+	
+	private Long id;
+	private Integer roomNumber;
+	private Integer price;
+	private Links links;
+	
+	public ReservationResponse(Integer roomNumber, Integer price) {
+		super();
+		this.roomNumber = roomNumber;
+		this.price = price;
+	}
+
+	public ReservationResponse() {
+		super();
+	}
+	
+	
+
+}
---
+++ full-stack-app-angular-spring-boot/src/main/java/nsimat/edu/learning/rest/ReservationResource.java
@@ -0,0 +1,33 @@
+package nsimat.edu.learning.rest;
+
+import java.time.LocalDate;
+
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.DateTimeFormat.ISO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import nsimat.edu.learning.model.response.ReservationResponse;
+
+@RestController
+@RequestMapping("/room/reservation/v1")
+public class ReservationResource {
+	
+	@RequestMapping(path="", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	public ResponseEntity<ReservationResponse> getAvailableRooms(
+			@RequestParam(value = "checkin")
+			@DateTimeFormat(iso = ISO.DATE)
+			LocalDate checkin, 
+			@RequestParam(value = "checkout")
+			@DateTimeFormat(iso = ISO.DATE)
+			LocalDate checkout){
+		
+		return new ResponseEntity<>(new ReservationResponse(), HttpStatus.OK)
+	}
+
+}
