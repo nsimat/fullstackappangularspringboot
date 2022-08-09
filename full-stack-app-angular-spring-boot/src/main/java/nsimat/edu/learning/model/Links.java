df6a8b897ab470e70f8b26834b3ab69306f11282
nsimat <nsimba_matondo@yahoo.fr>
2022-08-09 12:19:47 +0200

    Adding entities Links and Self

---
+++ full-stack-app-angular-spring-boot/src/main/java/nsimat/edu/learning/model/Links.java
@@ -0,0 +1,12 @@
+package nsimat.edu.learning.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class Links {
+	
+	private Self self;	
+
+}
---
+++ full-stack-app-angular-spring-boot/src/main/java/nsimat/edu/learning/model/Self.java
@@ -0,0 +1,12 @@
+package nsimat.edu.learning.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Self {
+	
+	private String ref;
+
+}
