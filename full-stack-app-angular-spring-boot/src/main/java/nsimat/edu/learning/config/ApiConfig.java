0f4ce487d063b4c06282f6bac84969744fd58b55
Michel Nsimba Matondo <nsimba_matondo@yahoo.fr>
2022-08-11 15:07:03 +0200

    Refactoring ApiConfig to handle ObjectMapper

--- full-stack-app-angular-spring-boot/src/main/java/nsimat/edu/learning/config/ApiConfig.java
+++ full-stack-app-angular-spring-boot/src/main/java/nsimat/edu/learning/config/ApiConfig.java
@@ -1,16 +1,28 @@
 package nsimat.edu.learning.config;
 
+import com.fasterxml.jackson.databind.Module;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class ApiConfig {
+	
+	@Bean
+	public Module dateTimeModule(){
+		return new JavaTimeModule();
+	}
 
 	@Bean
+	@Primary
 	public ObjectMapper objectMapper() {
+		ObjectMapper objectMapper = new ObjectMapper();
+		objectMapper.registerModule(new JavaTimeModule());
+		
 		return new ObjectMapper();
 	}
 	
